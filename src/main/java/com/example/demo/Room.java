882eb1943689052013c64657db20e485c5d40174
Norah Do <ndo33@gatech.edu>
2021-08-23 16:10:23 +0700

    Initialize POJO

---
+++ src/main/java/com/example/demo/Room.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+public class Room {
+    private String name;
+    private Status status;
+
+    public Room(String name, Status status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
